f96d6cb760c1e707816b6060788b4cdda1b84cf7
Hansel López <halm29799@gmail.com>
2023-11-25 03:50:16 -0600

    Add models and textures

--- RendererOpenGL.py
+++ RendererOpenGL.py
@@ -17,16 +17,23 @@ clock = pygame.time.Clock()
 rend = Renderer(screen)
 rend.setShaders(vertex_shader, fragment_shader)
 
-modelo = Model(
-    filename="models/monkey.obj", 
-    translate=glm.vec3(0, -1, -5), 
-    rotation=glm.vec3(-90, 0, 0), 
-    scale=glm.vec3(0.05, 0.05, 0.05)
-)
+modelos = [
+    Model("models/human.obj", glm.vec3(0, -1.5, -5), glm.vec3(0, 0, 0), glm.vec3(0.03, 0.03, 0.03)),
+    Model("models/monkey.obj", glm.vec3(0, -1.5, -5), glm.vec3(-90, 0, 0), glm.vec3(0.06, 0.06, 0.06)),
+    Model("models/stone.obj", glm.vec3(0, -0.5, -5), glm.vec3(-90, 0, 0), glm.vec3(0.5, 0.5, 0.5)),
+    Model("models/table.obj", glm.vec3(0, -1, -5), glm.vec3(0, 90, 0), glm.vec3(2, 2, 2)),
+    Model("models/thunderphone.obj", glm.vec3(0, 0, -5), glm.vec3(0, 0, 0), glm.vec3(0.05, 0.05, 0.05)),
+    Model("models/model.obj", glm.vec3(0, 0, -5), glm.vec3(0, 0, 0), glm.vec3(1.5, 1.5, 1.5)),
+]
 
-modelo.loadTexture("textures/monkey.bmp")
+modelos[0].loadTexture("textures/human.bmp")
+modelos[1].loadTexture("textures/monkey.bmp")
+modelos[2].loadTexture("textures/stone.bmp")
+modelos[3].loadTexture("textures/table.bmp")
+modelos[4].loadTexture("textures/thunderphone.bmp")
+modelos[5].loadTexture("textures/model.bmp")
 
-rend.scene.append(modelo)
+rend.scene.append(modelos[0])
 
 isRunning = True
 while isRunning:
@@ -40,6 +47,24 @@ while isRunning:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 isRunning = False
+            elif event.key == pygame.K_1:
+                rend.scene.clear()
+                rend.scene.append(modelos[0])
+            elif event.key == pygame.K_2:
+                rend.scene.clear()
+                rend.scene.append(modelos[1])
+            elif event.key == pygame.K_3:
+                rend.scene.clear()
+                rend.scene.append(modelos[2])
+            elif event.key == pygame.K_4:
+                rend.scene.clear()
+                rend.scene.append(modelos[3])
+            elif event.key == pygame.K_5:
+                rend.scene.clear()
+                rend.scene.append(modelos[4])
+            elif event.key == pygame.K_6:
+                rend.scene.clear()
+                rend.scene.append(modelos[5])
 
     if keys[K_d]:
         rend.camPosition.x += 5 * deltaTime
